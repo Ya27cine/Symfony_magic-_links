7f91ae5f5eeb052e2fd36778a2a8af64767f3c37
KHELIFA Yassine <prostam27@gmail.com>
2022-06-10 01:45:16 +0200

    install sys auth {Registration, Login}

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+     public function home(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/', name: 'main')]
+     public function index(): Response
+    {
+        return $this->redirectToRoute("app_home");
+    }
+}
