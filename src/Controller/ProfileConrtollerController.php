9e3427f98e071411023fc5f901e87c178e308fbc
KHELIFA Yassine <prostam27@gmail.com>
2022-06-11 03:56:17 +0200

    Une authentification MAGIQUE

---
+++ src/Controller/ProfileConrtollerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileConrtollerController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile_conrtoller')]
+    public function index(): Response
+    {
+        return $this->render('profile_conrtoller/index.html.twig', [
+            'controller_name' => 'ProfileConrtollerController',
+        ]);
+    }
+}
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\LoginLink\LoginLinkHandlerInterface;
 
 class SecurityController extends AbstractController
 {
@@ -28,5 +32,25 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/magic', name: 'magic')]
+    public function magic(UserRepository $userRepository,
+     LoginLinkHandlerInterface $loginLinkHandlerInterface,
+     MailerInterface $mailer){
+
+       $users = $userRepository->findAll();
+       foreach ($users as $user) {
+          $my_login_link = $loginLinkHandlerInterface->createLoginLink( $user );
+
+          $email = new Email();
+          $email->from("resetpassword@test.com")
+                ->to($user->getEmail())
+                ->subject("Link for Rest your password !!")
+                ->html( "<h2>Your magic link is :</h2> <a href='".$my_login_link->getUrl()."'> reset password . </a>")
+            ;
+          $mailer->send( $email );
+       }
+       return new Response("well done !", 200);
     }
 }
